391c1cd6db5a9305ba8f2cffdbe46c4f259903bc
bablysingh2409 <bablysingh2409@gmail.com>
2023-12-19 16:50:04 +0530

    Implemented startup search endpoint and controller in startupController

--- controller/startupController.js
+++ controller/startupController.js
@@ -26,28 +26,50 @@ const startupController = {
 
     },
 
-      // creating a new startup
-      createNewStartup:async(req,res)=>{
-          console.log(req.body);
-        try{
-            const data=req.body;
-            const newStartup=new Startup({
-                company_name:data.companyName,
-                city:data.city,
-                starting_year:data.startingYear,
-                founders:data.founders,
-                industry:data.industry,
-                funding_amount:data.fundingAmount
+    // creating a new startup
+    createNewStartup: async (req, res) => {
+        console.log(req.body);
+        try {
+            const data = req.body;
+            const newStartup = new Startup({
+                company_name: data.companyName,
+                city: data.city,
+                starting_year: data.startingYear,
+                founders: data.founders,
+                industry: data.industry,
+                funding_amount: data.fundingAmount
             })
-            const saveData=await newStartup.save();
+            const saveData = await newStartup.save();
             res.status(200).json(saveData);
-             
+
         }
-        catch(err){
+        catch (err) {
             console.error('Error creating a new startup:', err);
             res.status(500).json({ error: 'Internal Server Error' });
         }
+    },
+
+    //searching startups
+    searchStartups: async (req, res) => {
+        const { q } = req.query;
+        try {
+            const data = await Startup.find({
+                $or: [
+                    { company_name: { $regex: new RegExp(q, 'i') } },
+                    { industry: { $regex: new RegExp(q, 'i') }, },
+                    { founders: { $regex: new RegExp(q, 'i') }, }
+
+                ]
+            })
+            res.status(200).json(data);
+
+        }
+        catch (err) {
+            console.error('Error searching startups:', err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
     }
+
 
 
 }
--- routes/startupRoutes.js
+++ routes/startupRoutes.js
@@ -5,6 +5,7 @@ const startupController=require('../controller/startupController');
 
 router.get('/startups',startupController.getAllStartups);
 router.get('/startups/:industry',startupController.getStartupByIndustry);
+router.get('/search',startupController.searchStartups);
 router.post('/createstartup',startupController.createNewStartup);
 
 module.exports=router;
